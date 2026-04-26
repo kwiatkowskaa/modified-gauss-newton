1ff4b4318f11f50c6906aa596bcee689f97cb0f7
Katsairyna Bokhan <kateqwerty001@gmail.com>
2026-04-26 20:10:45 +0200

    modified gauss newton initial version completed. one problem - Thurber added and tested.

--- src/modified_gauss_newton.py
+++ src/modified_gauss_newton.py
@@ -1,5 +1,9 @@
+import numpy as np
+from subproblem import solve_subproblem
 
-def modified_gauss_newton(problem, x0, M0=1.0, L0=1e-6, max_iter=100, tol=1e-6):
+def modified_gauss_newton(problem, x0, M0=1.0, L0=1e-6, max_iter=100, tol=1e-6, M_search=2):
+    if M_search not in [1, 2]:
+        raise ValueError("M_search must be either 1 (M only grows) or 2 (M grows and shrinks).")
     
     # Initialization
     x=x0
@@ -7,8 +11,37 @@ def modified_gauss_newton(problem, x0, M0=1.0, L0=1e-6, max_iter=100, tol=1e-6):
     L=L0
 
     for k in range(max_iter):
-        
-        F_x = problem.F(x)
-        JF_x = problem.JF(x)
+        F_k = problem.F(x)
+        J_k = problem.J(x)
+
+        # M search
+        while True:
+            h = solve_subproblem(Fx = F_k, Jx = J_k, M = M)
+            candidate_point = x + h   # V_M_k
+            
+            f_candidate_real = np.linalg.norm(problem.F(candidate_point))   # f(V_M_k(x_k))
+            f_model = np.linalg.norm(F_k + J_k @ h) + 0.5 * M * np.linalg.norm(h)**2   # f_M_k(x_k)
+
+            if f_candidate_real <= f_model:
+                break
+            else:
+                M *= 2
+
+        x = candidate_point
+        if M_search == 1:
+            M = M
+        elif M_search == 2:
+            M = max(M * 0.5, L0)
+
+        if np.linalg.norm(h) < tol:
+            print(f"Converged in {k+1} iterations.")
+            break  
+
+    return x 
+
+
+
+
+
 
         
--- src/problem.py
+++ src/problem.py
@@ -1,12 +1,88 @@
-class Problem:
+import numpy as np
+from abc import ABC, abstractmethod
+
+class Problem(ABC):
+    def __init__(self, name, n, m):
+        self.name = name
+        self.n = n
+        self.m = m
+
+    @abstractmethod
     def F(self, x):
+        """Calculates the residual vector F(b) = model(x, b) - y. """
+        pass
+    
+    @abstractmethod
+    def J(self, x):
+        """ Calculates the Jacobian matrix J(b) = dF/db. """
         pass
 
-    def JF(self, x):
+    @abstractmethod
+    def get_starting_points(self):
+        """Returns the proposed starting points defined by NIST."""
         pass
 
-    def g(self, z):
-        pass
+    # def g(self, z):
+    #     pass
 
-    def grad_g(self, z):
-        pass
+    # def grad_g(self, z):
+    #     pass
+
+
+class ThurberProblem(Problem):
+    def __init__(self):
+        super().__init__(name="Thurber", n=7, m=37)
+        self.x_data = np.array([
+            -3.067, -2.981, -2.921, -2.912, -2.840, -2.797, -2.702, -2.699, 
+            -2.633, -2.481, -2.363, -2.322, -1.501, -1.460, -1.274, -1.212, 
+            -1.100, -1.046, -0.915, -0.714, -0.566, -0.545, -0.400, -0.309, 
+            -0.109, -0.103, 0.010, 0.119, 0.377, 0.790, 0.963, 1.006, 
+            1.115, 1.572, 1.841, 2.047, 2.200
+        ])
+
+        self.y_data = np.array([
+            80.574, 84.248, 87.264, 87.195, 89.076, 89.608, 89.868, 90.101, 
+            92.405, 95.854, 100.696, 101.060, 401.672, 390.724, 567.534, 
+            635.316, 733.054, 759.087, 894.206, 990.785, 1090.109, 1080.914, 
+            1122.643, 1178.351, 1260.531, 1273.514, 1288.339, 1327.543, 
+            1353.863, 1414.509, 1425.208, 1421.384, 1442.962, 1464.350, 
+            1468.705, 1447.894, 1457.628
+        ])
+
+    def model(self, b, x):
+        """
+        Rational cubic/cubic function.
+        y = (b1 + b2*x + b3*x^2 + b4*x^3) / (1 + b5*x + b6*x^2 + b7*x^3) 
+        """
+        numerator = b[0] + b[1]*x + b[2]*x**2 + b[3]*x**3
+        denominator = 1 + b[4]*x + b[5]*x**2 + b[6]*x**3
+        return numerator / denominator 
+
+    def F(self, b):
+        return self.model(b, self.x_data) - self.y_data
+    
+    def J(self, b):
+        x = self.x_data
+        numerator = b[0] + b[1]*x + b[2]*x**2 + b[3]*x**3    
+        denominator = 1 + b[4]*x + b[5]*x**2 + b[6]*x**3
+
+        denominator_squared = denominator ** 2
+
+        jacobian = np.zeros((self.m, self.n))
+
+        jacobian[:, 0] = 1 / denominator
+        jacobian[:, 1] = x / denominator
+        jacobian[:, 2] = x**2 / denominator
+        jacobian[:, 3] = x**3 / denominator
+
+        jacobian[:, 4] = - numerator * x / denominator_squared
+        jacobian[:, 5] = - numerator * x**2 / denominator_squared
+        jacobian[:, 6] = - numerator * x**3 / denominator_squared
+
+        return jacobian
+
+    def get_starting_points(self):
+        return {
+            "NIST Start 1": np.array([1000.0, 1000.0, 400.0, 40.0, 0.7, 0.3, 0.03]),
+            "NIST Start 2": np.array([1300.0, 1500.0, 500.0, 75.0, 1.0, 0.4, 0.05])
+        }

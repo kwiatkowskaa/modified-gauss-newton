f87525a83b0e812479411a9127f787d559cc9801
Katsairyna Bokhan <kateqwerty001@gmail.com>
2026-05-10 14:05:32 +0200

    small update - current version

--- optimization_project/newton_with_secular_equation.py
+++ optimization_project/newton_with_secular_equation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.linalg import cholesky, solve_triangular
 
 class NewtonSecularEquationSolver:
     """
@@ -16,7 +17,7 @@ class NewtonSecularEquationSolver:
         self.lam_U = None
         self.lam_start = None
 
-    def get_initial_bounds(self):
+    def _get_initial_bounds(self):
         """ (7.3.8 section) - page 192 """
         g_norm = np.linalg.norm(self.g)
         diag_H = np.diag(self.H)
@@ -41,25 +42,66 @@ class NewtonSecularEquationSolver:
         else:
             self.lam_start = max(self.lam_L, g_norm / self.delta)
             
-        return self.lam_L, self.lam_U, self.lam_start
+        return  self.lam_L, self.lam_U, self.lam_start
     
-    def find_lambda_uncertainty(self, method, lam_L, lam_U):
+    def _find_lambda_uncertainty(self, method, lam_L, lam_U):
         """ (7.3.6 section) - page 189 """
+        geo_mean = np.sqrt(lam_L * lam_U) 
+        theta = 0.01
+
         if method == "arithmetic_mean":
             return 0.5 * (lam_L + lam_U)
         
         elif method == "geometric_mean":
-            return np.sqrt(lam_L * lam_U) 
+            return geo_mean
         
         elif method == "theta_1":
-            theta = 0.01
-            return max(np.sqrt(lam_L*lam_U), lam_L+theta*(lam_U-lam_L))
+            return max(geo_mean, lam_L+theta*(lam_U-lam_L))
         
         elif method == "theta_2":
             theta = 0.01
-            return max(np.sqrt(lam_L*lam_U), theta*lam_U)
+            return max(geo_mean, theta*lam_U)
         
         raise ValueError(f"Unknown method: {method}")
+    
+    def _perform_step(self, H_lam):
+        """ Algorithm 7.3.1 """
+        # case: lam > - lam_1
+        try:
+            # we are in zone F of lambdas
+            L = cholesky(H_lam, lower=True)
+
+            # solve LL^T * s = -g
+            y = solve_triangular(L, -self.g, lower=True)
+            s = solve_triangular(L.T, y, lower=False)
+            s_norm = np.linalg.norm(s)
+
+            # solve (L*w = s)
+            w = solve_triangular(L, s, lower=True)
+            w_norm = np.linalg.norm(w)
+
+            return s, s_norm, w_norm, True
+        
+        except np.linalg.LinAlgError:
+            # we are in zone N of lambdas <--> cholesky functiorization is impossible
+            return None, None, None, False
+
+
+    # def solve(self):
+    #     lam_L, lam_U, lam = self._get_initial_bounds()
+
+    #     n = self.H.shape[0]
+    #     identity = np.eye(n)
+
+    #     # STEP 1: check for INTERIOR SOLUTION - page 174
+    #     s, s_norm, w_norm, is_pos_def = self._perform_step(self.H)
+
+    #     if is_pos_def and s_norm < self.delta:
+    #         # case lam is in G region
+    #         return 0.0, s
+
+
+
 
 
 
